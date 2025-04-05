df08b9b899f90badbf26f031ffbf93b838d0fe0b
mostafa <mostafawaelshoeip@gmail.com>
2025-04-05 20:49:29 +0200

    Add Files

---
+++ script.js
@@ -0,0 +1,44 @@
+// Create Variables
+const inputBox = document.getElementById("input-box");
+const listContainer = document.getElementById("list-container");
+
+// Add Task Function
+function addTask () {
+    // Alert to write anything
+    if(inputBox.value === '') {
+        alert("You Must Write Something!");
+    }
+
+    // Create notes
+    else {
+        let li = document.createElement("li");
+        li.innerHTML = inputBox.value;
+        listContainer.appendChild(li);
+
+        let span = document.createElement("span");
+        span.innerHTML = "\u00d7";
+        li.appendChild(span);
+    }   
+    inputBox.value = "";
+    saveData()
+}
+
+listContainer.addEventListener("click", function(e) {
+    if(e.target.tagName === "LI") {
+        e.target.classList.toggle("checked");
+        saveData()
+    }
+    else if(e.target.tagName === "SPAN") {
+        e.target.parentElement.remove();
+        saveData()
+    }
+}, false);
+
+// Local Storage to save changes
+function saveData() {
+    localStorage.setItem("data", listContainer.innerHTML);
+}
+function showTask() {
+    listContainer.innerHTML = localStorage.getItem("data");
+}
+showTask();
